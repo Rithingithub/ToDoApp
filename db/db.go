a51b53ea1160619a701e05a949186c2fbe39161a
LEHANS <latheeflathuthekkil@gmail.com>
2023-03-23 22:29:31 +0530

    feat: change models on mongodb

--- db/db.go
+++ db/db.go
@@ -9,16 +9,19 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func db() {
+func ConnectDB() (*mongo.Client, error) {
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()               // always remember to cancel the context to avoid leaking resources
-	defer client.Disconnect(ctx) // always remember to disconnect the client to avoid leaking resources
+	defer cancel() // always remember to cancel the context to avoid leaking resources
 
-	col := client.Database("test").Collection("users")
-	_ = col // to avoid a "declared but not used" error
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	log.Println("Connected to MongoDB!")
+	return client, nil
 }
